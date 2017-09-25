06ae949304cc85f823cefd4a21abe84599415007
dekkiller <dennissimon1993@gmail.com>
2017-09-25 16:21:44 -0700

    Testing VS debugger

---
+++ SimpleCalc/SimpleCalc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum;
+            sum = AddNumbers(62, 34);
+            Console.WriteLine(sum);
+            Console.WriteLine(AddNumbers(-1,10));
+            Console.ReadKey();
+        }
+
+        private static int AddNumbers(int numberOne, int numberTwo)
+        {
+            var calcResult = numberOne + numberTwo;
+            return calcResult;
+        }
+    }
+}
