03f0c697b687f9eac797257dbc29dd592d8f5c27
dekkiller <dennissimon1993@gmail.com>
2017-09-25 17:44:17 -0700

    added user input

--- SimpleCalc/SimpleCalc/Program.cs
+++ SimpleCalc/SimpleCalc/Program.cs
@@ -11,15 +11,40 @@ namespace SimpleCalc
         static void Main(string[] args)
         {
             int sum;
-            sum = AddNumbers(62, 34);
-            Console.WriteLine(sum);
-            Console.WriteLine(AddNumbers(-1,10));
+            Console.WriteLine("Enter function to do: Add, Subtract");
+            var userFunction = Console.ReadLine();
+
+            if (userFunction.ToLower() == "add")
+            {
+                Console.WriteLine("Enter the first number to add.");
+                var numberOne = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second numnber to add.");
+                var numberTwo = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(AddNumbers(numberOne, numberTwo));
+            }
+            else if (userFunction.ToLower() == "subtract")
+            {
+                Console.WriteLine("Enter the first number to sub from.");
+                var numberOne = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the second numnber to subtract from number 1.");
+                var numberTwo = Int32.Parse(Console.ReadLine());
+                Console.WriteLine(SubtractNumbers(numberOne, numberTwo));
+            }
+
             Console.ReadKey();
         }
 
         private static int AddNumbers(int numberOne, int numberTwo)
         {
+            Console.WriteLine();
             var calcResult = numberOne + numberTwo;
+            return calcResult;
+        }
+
+        private static int SubtractNumbers(int numberOne, int numberTwo)
+        {
+            Console.WriteLine();
+            var calcResult = numberOne - numberTwo;
             return calcResult;
         }
     }
